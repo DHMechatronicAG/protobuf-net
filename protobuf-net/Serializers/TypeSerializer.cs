b8096e8c9f9a63e61c8cc8835c990ea65b3610c7
Kevin Gardthausen <kevin.gardthausen@dh-partner.com>
2016-07-21 16:46:29 +0200

    Changed project setting for .Net Framework 4.0 and added SLN to open just that project.

--- protobuf-net/Serializers/TypeSerializer.cs
+++ protobuf-net/Serializers/TypeSerializer.cs
@@ -313,7 +313,7 @@ namespace ProtoBuf.Serializers
                 if (!hasConstructor) TypeModel.ThrowCannotCreateInstance(constructType);
                 obj = Activator.CreateInstance(constructType
 #if !(CF || SILVERLIGHT || WINRT || PORTABLE || NETSTANDARD1_3 || NETSTANDARD1_4)
-                    , nonPublic: true
+                    , true
 #endif
                     );
             }
